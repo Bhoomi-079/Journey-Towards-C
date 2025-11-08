0a78c9042b8401ad61be1a8441f12d917e3bfd4a
Bhoomi-079 <bhoomiasrani07@gmail.com>
2025-11-08 08:26:26 +0530

    Update type(48).c

--- vote(48).c
+++ vote(48).c
@@ -1,15 +1,13 @@
-#include <stdio.h>
-
-int main() {
-    int age;
-    printf("Enter your age: ");
-    scanf("%d", &age);
-
-    if (age >= 18) {
-        printf("You are eligible to vote.\n");
-    } else {
-        printf("You are not eligible to vote yet.\n");
-    }
-
-    return 0;
-}
+#include <stdio.h> 
+int main() { float side1, side2, side3; 
+ printf("Enter the lengths of the three sides of the triangle: ");
+ scanf("%f %f %f", &side1, &side2, &side3); 
+if (side1 > 0 && side2 > 0 && side3 > 0 && (side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1){ 
+printf("The sides form a valid triangle.\n");
+ 
+ if (side1 == side2 && side2 == side3) { 
+printf("It is an Equilateral triangle.\n"); } else if (side1 == side2 || side1 == side3 || side2 == side3) 
+{ printf("It is an Isosceles triangle.\n"); } else if
+ { printf("It is a Scalene triangle.\n"); } } else {
+printf("The given sides do not form a valid triangle.\n"); }
+ return 0; }
